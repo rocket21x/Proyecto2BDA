ee1d7462ef1cb8b4cd82b722f8ca7283713d350f
jesus imperial <jeskid99@gmail.com>
2024-04-03 17:51:53 -0700

    GUI

---
+++ GUI-Proyecto2/src/main/java/com/mycompany/gui/proyecto2/GUIProyecto2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gui.proyecto2;
+
+import Formularios.MenuGUI;
+
+/**
+ *
+ * @author jesus
+ */
+public class GUIProyecto2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        MenuGUI menu = new MenuGUI();
+        menu.setVisible(true);
+    }
+}
