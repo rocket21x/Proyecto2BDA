acf039d0936316a4540bd1b8bb8606275bf8b6f3
InkLao <eduardo.cornejo.balcazar@gmail.com>
2024-04-15 14:57:34 -0700

    Clean and Build

--- Persistencia-Proyecto2/target/generated-sources/annotations/DTOss/Persona_.java
+++ Persistencia-Proyecto2/target/generated-sources/annotations/DTOss/Persona_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-04-15T13:04:05", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-04-15T14:56:23", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Persona.class)
 public class Persona_ { 
 
